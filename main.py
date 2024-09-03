cd83f917ed3e3581e14b5550034b23bdaefa4268
Flávio E Pimentel <63177834+flavioepimentel@users.noreply.github.com>
2024-09-03 01:08:47 -0300

    feat :sparkles: Implementa saque, depósito e extrato
    Recurso segue regras de valor máximo de sáque, quantidade de saques diários além de controle de saldo.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-    DIO BANK É UMA APLICA
+    DIO BANK É UMA APLICAÇÃO QUE TEM O OBJETIVO DE DEMONSTRAR OPERAÇÕES BÁSICAS DE UM APP DE BANCO
 """
 
 import json
@@ -16,21 +16,13 @@ menu = """
 => """
 
 
-SALDO = 0
-LIMITE_VALOR_SAQUE = 500
-CONTAGEM_SAQUES_DIA = 0
-LIMITE_NUMERO_SAQUES = 3
+SALDO_SESSION: float = 0.0
+LIMITE_VALOR_SAQUE: float = 500.0
+CONTAGEM_SAQUES_DIA: int = 0
+LIMITE_NUMERO_SAQUES: int = 3
 
 
-def write_operation(json_string) -> None:
-    """
-    Se existir o arquivo ele escreve um texto novo e se não existir cria e escreve o arquivo
-    """
-    with open("data.json", "w") as file:
-        file.write(json_string)
-
-
-def read_operation():
+def read_operation() -> str:
     """
     Realiza a tentativa de ler o arquivo e se não encontrado cria o arquivo com uma lista vazia
     """
@@ -38,17 +30,84 @@ def read_operation():
         with open("data.json", "r") as file:
             return file.read()
     except FileNotFoundError:
-        write_operation("[]")
+        return "[]"
 
+
+def deserialize_bank_statement() -> list:
+    """
+    Realiza a desserialização do arquivo json com os dados do extrato de string para objeto/dicionário (dict) do Python
+    caso o arquivo seja encontrado em branco, ele devolve uma lista vazia para inicio dos registros do extrato.
+    """
+    if read_operation() != '':
+        return json.loads(read_operation())
+    else:
+        return []
+
+
+def write_operation(json_string) -> None:
+    """
+    Se existir o arquivo ele escreve um texto novo e se não existir cria e escreve o arquivo
+    """
+    historical_loads: list = deserialize_bank_statement()
+    historical_loads.append(json_string)
+    historical_dumps: str = json.dumps(historical_loads, default=str)
+    with open("data.json", "w") as file:
+        file.write(historical_dumps)
+
+
+# "2024-09-02 23:00:44.480308"
 
 def deposit_operation(value) -> None:
-    global SALDO
-    SALDO += value
+    """
+    Realiza a operação de soma na variável global SALDO_SESSION e a escrita da operação no extrato
+    """
+    global SALDO_SESSION
+    SALDO_SESSION += value
+    write_operation({
+        "timestamp": datetime.datetime.now(),
+        "operation": "Deposito",
+        "value": value,
+        "city": "New York"
+    })
 
 
 def withdrawal_operation(value) -> None:
-    global SALDO
-    SALDO -= value
+    """
+    Realiza a operação de subtração na variável global SALDO_SESSION e a escrita da operação no extrato
+    """
+    global SALDO_SESSION
+    SALDO_SESSION -= value
+    write_operation({
+        "timestamp": datetime.datetime.now(),
+        "operation": "Saque",
+        "value": value,
+        "city": "New York"
+    })
+
+
+def load_bank_balance():
+    bank_statement: object | list = deserialize_bank_statement()
+    saldo: float = 0.0
+
+    for i, v in enumerate(bank_statement):
+
+        if bank_statement[i]["operation"] == "Deposito":
+            saldo += float(bank_statement[i]["value"])
+
+        elif bank_statement[i]["operation"] == "Saque":
+            saldo -= float(bank_statement[i]["value"])
+
+    return saldo
+
+
+def count_withdrawal_by_date():
+    bank_statement: object | list = deserialize_bank_statement()
+    withdrawal = 0
+    for i, v in enumerate(bank_statement):
+        if (bank_statement[i]["operation"] == "Saque") and (
+                datetime.datetime.strptime(bank_statement[i]["timestamp"], "%Y-%m-%d %H:%M:%S.%f").day == datetime.datetime.now().day):
+            withdrawal += 1
+    return withdrawal
 
 
 if __name__ == '__main__':
@@ -60,28 +119,17 @@ if __name__ == '__main__':
             valor = float(input("Informe o valor do depósito: "))
 
             if valor > 0:
-                extrato = read_operation()
                 deposit_operation(valor)
-                ct = datetime.datetime.now()
-
-                data = {
-                    "timestamp": ct,
-                    "operation": "Deposito",
-                    "value": valor,
-                    "city": "New York"
-                }
-
-                write_operation(data)
-
-                extrato += f"Depósito: R$ {valor:.2f}\n"
 
             else:
                 print("Operação falhou! O valor informado é inválido.")
 
         elif opcao == "s":
             valor = float(input("Informe o valor do saque: "))
+            saldo = load_bank_balance()
+            CONTAGEM_SAQUES_DIA = count_withdrawal_by_date()
 
-            excedeu_saldo = valor > SALDO
+            excedeu_saldo = valor > saldo
 
             excedeu_limite = valor > LIMITE_VALOR_SAQUE
 
@@ -94,30 +142,37 @@ if __name__ == '__main__':
                 print("Operação falhou! O valor do saque excede o limite.")
 
             elif excedeu_saques:
-                print("Operação falhou! Número máximo de saques excedido.")
+                print(
+                    """\nOperação falhou! Você excedeu o número máximo de saques diários. 
+Tente novamente amanhã.""")
+                print("Saques realizados: ", CONTAGEM_SAQUES_DIA)
 
             elif valor > 0:
                 withdrawal_operation(valor)
-                ct = datetime.datetime.now()
-
-                data = {
-                    "timestamp": ct,
-                    "operation": "John",
-                    "value": 30,
-                    "city": "New York"
-                }
-
-                extrato += f"Saque: R$ {valor:.2f}\n"
-                CONTAGEM_SAQUES_DIA += 1
 
             else:
                 print("Operação falhou! O valor informado é inválido.")
 
         elif opcao == "e":
-            print("\n================ EXTRATO ================")
-            print("Não foram realizadas movimentações." if not extrato else extrato)
-            print(f"\nSaldo: R$ {SALDO:.2f}")
-            print("==========================================")
+            extrato = deserialize_bank_statement()
+            saldo = load_bank_balance()
+            if extrato == []:
+                print("\n================ EXTRATO ================\n")
+                print("Não foram realizadas movimentações.")
+                print(f"\nSaldo: R$ {saldo:.2f}")
+                print("==========================================")
+            else:
+                historico = ""
+                for i, v in enumerate(extrato):
+                    historico += f'{extrato[i]["operation"]
+                                    } - R$ {extrato[i]["value"]:.2f}\n'
+
+                print("\n================ EXTRATO ================\n")
+
+                print(historico)
+
+                print(f"\nSaldo: R$ {saldo:.2f}")
+                print("==========================================")
 
         elif opcao == "q":
             break
